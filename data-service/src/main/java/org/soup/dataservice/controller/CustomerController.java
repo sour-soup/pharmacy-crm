4596089f84d2f8484ca17e28e106cb5ac12c8ffa
sour-soup <kiziyepi8@gmail.com>
2024-12-03 15:01:33 +0400

    fix endpoints

--- data-service/src/main/java/org/soup/dataservice/controller/CustomerController.java
+++ data-service/src/main/java/org/soup/dataservice/controller/CustomerController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/customers")
+@RequestMapping("/api/customers/")
 public class CustomerController extends DetailsController<Customer, CustomerRepository> {
     public CustomerController(CustomerRepository repository) {
         super(repository);
